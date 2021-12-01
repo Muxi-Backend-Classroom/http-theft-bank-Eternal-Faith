8d0245fbaf38f682e3a8a0f2aff2d1457d44f93f
Eternal-Faith <92308359+Eternal-Faith@users.noreply.github.com>
2021-12-01 11:31:05 +0800

    Update main.go

--- checkpoint1/main.go
+++ checkpoint1/main.go
@@ -20,4 +20,10 @@ func main() {
 
 	// write your code below
 	// ...
+	client := &http.Client{}
+	req, _ := http.NewRequest("GET","http://http-theft-bank.gtainccnu.muxixyz.com/api/v1/organization/code",nil)
+	req.Header.Add("Code","120")			
+	res, _ := client.Do(req)				//Do发送请求返回http回复
+	by, _ := ioutil.ReadAll(res.Body)		//从res.body读取数据直到遇到error或EOF
+	fmt.Println(string(by))	
 }
