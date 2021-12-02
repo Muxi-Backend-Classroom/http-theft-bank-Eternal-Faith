371034ee44f0fd7faaf5e71836ddd3d00b3d608f
Eternal-Faith <92308359+Eternal-Faith@users.noreply.github.com>
2021-12-02 21:37:28 +0800

    Update main.go

--- checkpoint1/main.go
+++ checkpoint1/main.go
@@ -6,17 +6,17 @@ import (
 )
 
 func main() {
-	req, err := httptool.NewRequest(
-		"",
-		"",
-		"",
-		httptool.DEFAULT, // 这里可能不是 DEFAULT，自己去翻阅文档
-	)
-	if err != nil {
-		fmt.Println(err)
-	}
+// 	req, err := httptool.NewRequest(
+// 		"",
+// 		"",
+// 		"",
+// 		httptool.DEFAULT, // 这里可能不是 DEFAULT，自己去翻阅文档
+// 	)
+// 	if err != nil {
+// 		fmt.Println(err)
+// 	}
 
-	fmt.Println(req)
+// 	fmt.Println(req)
 
 	// write your code below
 	// ...
