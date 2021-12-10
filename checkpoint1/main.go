a7e0ead782a1cda9c71722f41038f1c4e15226d3
Eternal-Faith <92308359+Eternal-Faith@users.noreply.github.com>
2021-12-10 09:19:31 +0800

    Update main.go

--- checkpoint1/main.go
+++ checkpoint1/main.go
@@ -9,7 +9,7 @@ import (
 func main() {	
 	client := &http.Client{}
 	req, _ := http.NewRequest("GET", "http://http-theft-bank.gtainccnu.muxixyz.com/api/v1/organization/code", nil)
-	req.Header.Add("Code", "120")
+	req.Header.Add("Code", "123")
 	res, _ := client.Do(req)
 	by, _ := ioutil.ReadAll(res.Body)
 	fmt.Println(string(by))	
