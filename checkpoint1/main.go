932791654d3fc8f3154d890e8771d47411771dab
Eternal-Faith <92308359+Eternal-Faith@users.noreply.github.com>
2021-12-10 08:59:50 +0800

    Update main.go

--- checkpoint1/main.go
+++ checkpoint1/main.go
@@ -2,28 +2,17 @@ package main
 
 import (
 	"fmt"
-	"github.com/Grand-Theft-Auto-In-CCNU-MUXI/hacker-support/httptool"
+	"io/ioutil"
+	"net/http"
 )
 
 func main() {
-// 	req, err := httptool.NewRequest(
-// 		"",
-// 		"",
-// 		"",
-// 		httptool.DEFAULT, // 这里可能不是 DEFAULT，自己去翻阅文档
-// 	)
-// 	if err != nil {
-// 		fmt.Println(err)
-// 	}
-
-// 	fmt.Println(req)
-
-	// write your code below
-	// ...
+	
 	client := &http.Client{}
-	req, _ := http.NewRequest("GET","http://http-theft-bank.gtainccnu.muxixyz.com/api/v1/organization/code",nil)
-	req.Header.Add("Code","120")			
-	res, _ := client.Do(req)				//Do发送请求返回http回复
-	by, _ := ioutil.ReadAll(res.Body)		//从res.body读取数据直到遇到error或EOF
-	fmt.Println(string(by))	
+	req, _ := http.NewRequest("GET", "http://http-theft-bank.gtainccnu.muxixyz.com/api/v1/organization/code", nil)
+	req.Header.Add("Code", "123")
+	res, _ := client.Do(req)
+	by, _ := ioutil.ReadAll(res.Body)
+	fmt.Println(string(by))
+	
 }
